a40cf6a7b4de02285da0b96ff812e59ed3b0c311
bement2050 <bemnetgizachew@gmail.com>
2021-08-22 17:50:41 -0500

    'thirdcommit'

---
+++ app3.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request ,jsonify
+  
+app = Flask(__name__)
+  
+@app.route('/postjson', methods = ['POST'])
+def postJsonHandler():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return jsonify(content['device'])
+
+@app.route('/query-example')
+def query_example():
+    language = request.args.get('language') #if key doesn't exist, returns None
+    framework = request.args['framework'] #if key doesn't exist, returns a 400, bad request error
+    website = request.args.get('website')
+
+    return '''<h1>The language value is: {}</h1>
+              <h1>The framework value is: {}</h1>
+              <h1>The website value is: {}'''.format(language, framework, website)
+              
+              
+@app.route('/form-example', methods=['GET', 'POST']) #allow both GET and POST requests
+def form_example():
+    if request.method == 'POST':  #this block is only entered when the form is submitted
+        language = request.form.get('language')
+        framework = request.form['framework']
+
+        return '''<h1>The language value is: {}</h1>
+                  <h1>The framework value is: {}</h1>'''.format(language, framework)
+
+    return '''<form method="POST">
+                  Language: <input type="text" name="language"><br>
+                  Framework: <input type="text" name="framework"><br>
+                  <input type="submit" value="Submit"><br>
+              </form>'''
+app.run(host='0.0.0.0',debug=True)
